23f239d0b8c62cc425278cb2aea9c543d948feb7
eloudsa <said@noratek.net>
2020-02-23 13:49:09 +0100

    several examples

--- src/components/comp-a/comp-a.js
+++ src/components/comp-a/comp-a.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import styles from './comp-a.module.css';
-import './comp-a.css';
 
 export const CompA = () => (
   <div className={styles.wrapper}>
     <h3 className={styles.title}>Component A</h3>
     <button className={styles.button}>Click</button>
 
-    <div className='row'>
-      <div className={`col s12 m3 ${styles.projectAdditional}`}>
-        <ul style={{ margin: '-10px 0 0 0' }}>
+    <div className={`row ${styles.custom}`}>
+      <div
+        className={`col s12 m12 ${styles.projectAdditional} ${styles.column}`}
+      >
+        <ul className={styles.noBullet}>
           <li style={{ fontWeight: '600' }}>
             <i className={styles.materialIcons}>perm_data_setting</i>Beginner{' '}
             <span>difficulty</span>
@@ -19,7 +20,23 @@ export const CompA = () => (
             <span>days remaining</span>
           </li>
           <li style={{ fontWeight: '600' }}>
-            <i className={styles.materialIcons}>thumb_up</i>0 <span>submissions</span>
+            <i className={styles.materialIcons}>thumb_up</i>0{' '}
+            <span>submissions</span>
+          </li>
+        </ul>
+      </div>
+      <div className={`col s12 m12 ${styles.column}`}>
+        <ul className={styles.noBullet}>
+          <li>
+            <i className={styles.materialIcons}>add</i>
+            <span>Add</span>
+          </li>
+        </ul>
+      </div>
+      <div className={`col s12 m12 ${styles.column}`}>
+        <ul className={styles.noBullet}>
+          <li>
+            <span>Text</span>
           </li>
         </ul>
       </div>
